9e9c048296efe1a1a7a7a6ae6f0c9d6fb656d14b
aragonne <85344292+aragonne@users.noreply.github.com>
2022-04-12 13:02:31 +0200

    Update RunTest.java

--- src/test/java/org/cucumber/RunTest.java
+++ src/test/java/org/cucumber/RunTest.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"classpath:/src/test/resources/squash/test.feature"}, //NOTE: NO SPACE
+        )
 public class RunTest {
- features = {"classpath:/src/test/resources/squash/test.feature"},
+ 
  }
