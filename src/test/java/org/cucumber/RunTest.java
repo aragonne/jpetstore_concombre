374c9c751198dafe65c85364827c7c0cb534201c
aragonne <85344292+aragonne@users.noreply.github.com>
2022-04-12 12:54:47 +0200

    Update RunTest.java

--- src/test/java/org/cucumber/RunTest.java
+++ src/test/java/org/cucumber/RunTest.java
@@ -1,9 +1,9 @@
 package org.cucumber;
 
-import cucumber.api.junit.Cucumber;
+
 import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 public class RunTest {
-
-}
+ }
