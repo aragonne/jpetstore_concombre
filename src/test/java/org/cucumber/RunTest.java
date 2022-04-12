97112f998c2f106709cf204dee5d870dd101deab
aragonne <85344292+aragonne@users.noreply.github.com>
2022-04-12 17:24:58 +0200

    Update RunTest.java

--- src/test/java/org/cucumber/RunTest.java
+++ src/test/java/org/cucumber/RunTest.java
@@ -4,13 +4,14 @@ package org.cucumber;
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
-public class RunTest {
- @Cucumber.Options(
+@RunWith(Cucumber.class) 
+@Cucumber.Options(
 
 glue = {"org.cucumber.AnnotationSteps"},
 features = "~/test4/src/test/resources",
 format = {"pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json"}
 
 )
- }
+public class RunTest {
+
+}
